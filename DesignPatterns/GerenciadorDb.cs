406ac33d3bac907bd186d0a2f99f53ecd57bd648
Gustavo Roberto <gustavo.rmattos@gmail.com>
2021-04-16 19:58:44 -0300

    Codificação de exemplo do Design Pattern Singleton

---
+++ DesignPatterns/GerenciadorDb.cs
@@ -0,0 +1,25 @@
+﻿
+namespace DesignPatterns
+{
+    public class GerenciadorDb
+    {
+        private static GerenciadorDb _gerenciadorDb = null;
+
+        private GerenciadorDb()
+        {
+        }
+
+        public static GerenciadorDb GetInstance()
+        {
+            if(_gerenciadorDb == null)
+            {
+                _gerenciadorDb = new GerenciadorDb();
+                return _gerenciadorDb;
+            }
+
+            return _gerenciadorDb;
+        }
+
+        public string Propriedade { get; set; } = "String Padrão";
+    }
+}
--- DesignPatterns/Program.cs
+++ DesignPatterns/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.InteropServices;
 
 namespace DesignPatterns
 {
@@ -6,7 +7,20 @@ namespace DesignPatterns
     {
         static void Main(string[] args)
         {
-             
+            var gerenciador1 = GerenciadorDb.GetInstance();
+
+            Console.WriteLine("gerenciador1: " + gerenciador1.Propriedade);
+
+            Console.WriteLine("------------------------------------------------------");
+
+            GerenciadorDb gerenciador2 = GerenciadorDb.GetInstance();
+            Console.WriteLine("Modificação da propriedade pelo gerenciador2");
+
+            gerenciador2.Propriedade = "Nova string - modificação pelo gerenciador2";
+            
+            Console.WriteLine("gerenciador1: " + gerenciador1.Propriedade);
+
+            Console.WriteLine("\n\nConclusão: Sempre está sendo fornecida a mesma instância de memória da classe GerenciadorDb");
         }
     }
 }
