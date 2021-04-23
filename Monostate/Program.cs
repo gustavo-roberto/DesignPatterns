5d03ae2ee339fd88195a4ac49cd1b691f2e230af
Gustavo Roberto <60558697+gustavo-roberto@users.noreply.github.com>
2021-04-22 22:17:43 -0300

    Criação de exemplo do padrão Monostate (#2)

---
+++ Monostate/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Monostate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var spooler1 = new SpoolerImpressao();
+
+            spooler1.AdicionaArquivo(new Arquivo("arquivo1", ".txt", 16));
+            ListarArquivos(spooler1.RetornarSpooler());
+
+            var spooler2 = new SpoolerImpressao();
+
+            spooler2.AdicionaArquivo(new Arquivo("arquivo2", ".csv", 18));
+            ListarArquivos(spooler2.RetornarSpooler());
+            //Como pode-se ver no resultado do console, o atributo foi compartilhado entre as instâncias da classe SpoolerImpressao
+            //Assim, reproduziu-se o comportamento do Singleton e respeitou-se o princípio de responsabilidade única do SOLID
+        }
+
+        static void ListarArquivos(List<Arquivo> spooler)
+        {
+            foreach(var arquivo in spooler)
+            {
+                Console.WriteLine("Arquivo: " + arquivo.Nome);
+            }
+
+            Console.WriteLine("------------------------------");
+        }
+    }
+}
---
+++ Monostate/SpoolerImpressao.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Monostate
+{
+    public class SpoolerImpressao
+    {   //Segundo o padrão Monostate, todos os atributos devem ser privados e estáticos
+        private static List<Arquivo> _spooler = new List<Arquivo>();
+        //Já os seus setters e getters, devem ser públicos. Assim se garante um comportamento análogo ao Singleton
+        public void AdicionaArquivo(Arquivo arquivo)
+        {
+            _spooler.Add(arquivo);
+        }
+
+        public List<Arquivo> RetornarSpooler()
+        {
+            return _spooler;
+        }
+    }
+}
