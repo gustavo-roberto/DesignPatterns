ae1c383f7e151b4131f37590e42830e9fef59106
Gustavo Roberto <gustavo.rmattos@gmail.com>
2021-04-22 22:07:16 -0300

    Criação de exemplo do padrão Monostate

---
+++ Monostate/Arquivo.cs
@@ -0,0 +1,16 @@
+﻿namespace Monostate
+{
+    public class Arquivo
+    {
+        public string Nome { get; set; }
+        public string Tipo { get; set; }
+        public double TamanhoEmMb { get; set; }
+
+        public Arquivo(string nome, string tipo, double tamanho)
+        {
+            Nome = nome;
+            Tipo = tipo;
+            TamanhoEmMb = tamanho;
+        }
+    }
+}
